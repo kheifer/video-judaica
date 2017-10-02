8d3c0a94cc111c4be518df41bbbedd7556be4eb6
Guest User <Guest@epicodus.local>
2017-10-02 13:56:18 -0700

    add file layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { AboutComponent } from './about/about.component';
+import { ROUTING } from './app.routing';
+import { TitleListComponent } from './title-list/title-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    AboutComponent,
+    TitleListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
