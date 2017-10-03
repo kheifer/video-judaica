9fc77d66a87f0e76f707a6f15b003ba0b9d05350
Kheifer Fuller <Guest@epicodus.local>
2017-10-03 12:52:06 -0700

    add formsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
 import { masterFirebaseConfig } from  './api-kays';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule} from 'angularfire2/database';
@@ -31,6 +34,8 @@ export const firebaseConfig = {
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpModule,
     ROUTING,
     AngularFireModule.initializeApp(firebaseConfig),
     AngularFireDatabaseModule,
