8f4058dd4ce222c3d92713f8fb7c98a84a5ef2af
Esti Shay <esti.r.shay@gmail.com>
2017-10-05 14:56:20 -0700

    resolve detail conflicts

--- src/app/movie.model.ts
+++ src/app/movie.model.ts
@@ -1,8 +1,4 @@
 export class Movie {
-<<<<<<< HEAD
   public availability: string = "Available";
   constructor(public title: string, public runtime: string, public rating: string, public language: string, public genre: string, public format: string, public deposit: string, public imdb: string, public restriction: string){}
-=======
-  constructor(public title: string, public runtime: string, public rating: string, public language: string, public genre: string, public format: string, public availability: string, public deposit: string, public imdb: string, public restriction: string){}
->>>>>>> development
 }
--- src/app/title-detail/title-detail.component.ts
+++ src/app/title-detail/title-detail.component.ts
@@ -27,7 +27,6 @@ export class TitleDetailComponent implements OnInit {
                                         dataLastEmittedFromObserver.language,
                                         dataLastEmittedFromObserver.genre,
                                         dataLastEmittedFromObserver.format,
-                                        dataLastEmittedFromObserver.availability,
                                         dataLastEmittedFromObserver.deposit,
                                         dataLastEmittedFromObserver.imdb,
                                         dataLastEmittedFromObserver.restriction)
