82f1e1021e811331c28b9efc92110c8973396ea8
Kheifer <Guest@Epicodus-6A.local>
2017-10-05 10:19:22 -0700

    update title-list to work with database

--- src/app/title-list/title-list.component.ts
+++ src/app/title-list/title-list.component.ts
@@ -1,25 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
 
 import { MovieService } from '../movie.service';
 import { Movie } from '../movie.model';
 import { FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
-  selector: 'title-list',
+  selector: 'app-title-list',
   templateUrl: './title-list.component.html',
   styleUrls: ['./title-list.component.css'],
   providers: [MovieService]
 })
 export class TitleListComponent implements OnInit {
   currentRoute: string = this.router.url;
-  movies: FirebaseListObservable<any[]>;
+  movies: any[];
 
   constructor(private router: Router, private movieService: MovieService) { }
 
 
-  ngOnInit() {
-    this.movies = this.movieService.getMovies();
+  ngOnInit(){
+   this.movieService.getMovies().subscribe( newList => {
+      this.movies = newList;
+    });
   }
 
 }
